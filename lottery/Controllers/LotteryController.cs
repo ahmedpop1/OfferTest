b17158fa4f7133c503cc99e4ae010d9a5e41fe59
Ahmed Elakabawy <48593343+ahmedpop1@users.noreply.github.com>
2023-10-02 04:12:55 +0300

    Complete

---
+++ lottery/Controllers/LotteryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lottery.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LotteryController : ControllerBase
+    {
+        [Route("play")]
+        [HttpPost]
+        public int play(int customerNumber)
+        {
+           
+            return 1234;
+        }
+
+        }
+}
