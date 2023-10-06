f45af370eb789f0e900995cfef00c6bf4ef9e85e
Ahmed Elakabawy <48593343+ahmedpop1@users.noreply.github.com>
2023-10-06 19:07:37 +0300

    Complete Unit Testing

--- Chess.nUnitTests/ChessTests.cs
+++ Chess.nUnitTests/ChessTests.cs
@@ -5,16 +5,24 @@ namespace Chess.nUnitTests
     public class ChessTests
     {
         private Position _Position=new Position();
+        private Position _Position2 = new Position();
+
         [SetUp]
         public void Setup()
         {
              _Position = new Position() { X=1,Y=1};
+            _Position2 = new Position() { X=2,Y=2};
         }
 
         [Test]
         public void GetChess_equal_Movements()
         {
             Assert.AreEqual("Places are : 2,3|3,2", "Places are : " + _Position.GetAvailablePositions());
+        }
+        [Test]
+        public void GetChess2_equal_Movements()
+        {
+            Assert.AreEqual("Places are : 3,4|4,3|4,1|1,4", "Places are : " + _Position2.GetAvailablePositions());
         }
     }
 }
